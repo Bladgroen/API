34230f0c813c34b47cf1f654f98e85148b872806
Vincent Laureys <vincent.laureys@student.odisee.be>
2022-07-25 11:23:45 +0200

    m

--- dashboard/src/api/task-time/controllers/task-time.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  task-time controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::task-time.task-time');
--- dashboard/src/api/task-time/routes/task-time.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * task-time router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::task-time.task-time');
